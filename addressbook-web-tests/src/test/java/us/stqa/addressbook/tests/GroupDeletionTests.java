285ca6783775e289201ed33ad79ece509de44e74
nodir-sultan <nodirsm23@gmail.com>
2018-05-11 20:10:52 -0400

    added assertion of group lists before deletion and after deletion

--- addressbook-web-tests/src/test/java/us/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/us/stqa/addressbook/tests/GroupDeletionTests.java
@@ -11,7 +11,7 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().goToGroupPage();
-    if (! app.getGroupHelper().isThereGroup()){
+    if (!app.getGroupHelper().isThereGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Samplegroup1", "Samplegroup2", "Samplegroup3"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
@@ -21,5 +21,30 @@ public class GroupDeletionTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
   }
+
+  @Test
+  public void testGroupDeletionModified() {
+    app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Samplegroup1", "Samplegroup2", "Samplegroup3"));
+    }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(after, before);
+
+    // Assert.assertEquals(after.size(), before.size() - 1);
+    // loop for groupdata objects comparison
+    /*    before.remove(before.size() - 1);
+    for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before.get(i), after.get(i));
+    }*/
+  }
+
 
 }
